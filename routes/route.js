36a367b022edad4fbc0784cc596aa13c9cb6a1ce
Arijit Das <arijit10das2002@gmail.com>
2022-10-13 22:22:33 +0530

    .

--- routes/route.js
+++ routes/route.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { test, createUser } = require('../controllers/controller');
 
 router.get('/', test);
-//router.post('/create', createUser);
+router.post('/create', createUser);
 
 module.exports =router;
