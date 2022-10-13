88d6e9021417404afd8d075821937b3b10abb07a
Arijit Das <arijit10das2002@gmail.com>
2022-10-13 22:35:30 +0530

    createuser

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,12 +1,14 @@
 const mongooose = require('mongoose');
+const User = require('../model/user');
 
 module.exports.test =(req, res) => {
     res.send("working");
 };
 
-module.exports.createUser = (req, res) => {
+module.exports.createUser = async (req, res) => {
     const { first_name, last_name, city } = req.body;
     console.log(req.body);
+    await new User({first_name,last_name,city}).save();
     res.send("create user");
 };
 
