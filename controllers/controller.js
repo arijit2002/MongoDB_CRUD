d647784f95a75a250290583167222671e95fc754
Arijit Das <arijit10das2002@gmail.com>
2022-10-14 10:59:49 +0530

    getallusers

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,8 +7,11 @@ module.exports.test =(req, res) => {
 
 module.exports.createUser = async (req, res) => {
     const { first_name, last_name, city } = req.body;
-    console.log(req.body);
     await new User({first_name,last_name,city}).save();
     res.send("create user");
 };
 
+module.exports.getUsers = async (req, res) => {
+    const allUsers = await User.find();
+    res.send(allUsers);
+};
--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { test, createUser } = require('../controllers/controller');
+const { test, createUser, getUsers } = require('../controllers/controller');
 
 router.get('/', test);
 router.post('/create', createUser);
+router.get('/getUsers', getUsers);
 
 module.exports =router;
