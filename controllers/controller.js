2303a69a5d4a0888b35dfcae6f29ddfbaecc442d
Arijit Das <arijit10das2002@gmail.com>
2022-10-14 11:47:52 +0530

    delete by id

--- controllers/controller.js
+++ controllers/controller.js
@@ -15,3 +15,18 @@ module.exports.getUsers = async (req, res) => {
     const allUsers = await User.find();
     res.send(allUsers);
 };
+
+module.exports.getUserById = async (req, res) => {
+    const id = req.body.id;
+    const userPresent = await User.findById(id);
+    if(userPresent!=null) {res.send(userPresent);}
+    else res.send({"message":"user not present"});
+};
+
+module.exports.deleteUserById = async (req, res) => {
+    const id = req.body.id;
+    const userPresent = await User.findByIdAndRemove(id);
+    if(userPresent!=null) {
+        res.send({"message":"user deleted"});
+    }else res.send({"message":"user not present"});
+};
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { test, createUser, getUsers } = require('../controllers/controller');
+const { test, createUser, getUsers, getUserById, deleteUserById } = require('../controllers/controller');
 
 router.get('/', test);
 router.post('/create', createUser);
 router.get('/getUsers', getUsers);
+router.get('/getUser', getUserById);
+router.delete('/deleteUser', deleteUserById);
 
 module.exports =router;
