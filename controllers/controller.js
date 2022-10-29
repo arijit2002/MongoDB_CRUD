789b6838e435be32b039d67a567a9d838836f122
Arijit Das <arijit10das2002@gmail.com>
2022-10-29 20:09:00 +0530

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,7 @@
 const mongooose = require('mongoose');
 const User = require('../model/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports.login = async (req, res) => {
     const { email, password } = req.body;
